a0c828768c3baedf43780d64216f3e8fc1c60d82
jose211295@gmail.com <Alberche23>
2020-06-16 19:19:28 +0200

    añadidir url a las pruebas

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+
 import {PruebasService} from '../pruebas-service';
 import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
 import {Geolocation} from '@ionic-native/geolocation/ngx';
